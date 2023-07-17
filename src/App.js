cc19cabdf4966a02f89070b763902cf6ab4c141a
fulya_ture <fulyature@gmail.com>
2023-07-17 12:33:34 +0300

    initialized

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LifeCycleMethods from "./components/LifeCycleMethods";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        <LifeCycleMethods />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
